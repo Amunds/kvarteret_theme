58181c4c6a0c7739715456e9aaf80ae8024dd81e
Amund <amunds@gmail.com>
2010-09-30 17:55:28 +0200

    initial

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage kvarteret
+ * @since Kvarteret 1.0
+ */
+?>
+	</div><!-- #main -->
+
+	<div id="footer" role="contentinfo">
+	</div><!-- #footer -->
+
+<?php
+	wp_footer();
+?>
+</body>
+</html>
