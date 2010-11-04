07c04e21982f5d468323859e4f48ce5d0ff37a32
Amund <amunds@gmail.com>
2010-11-04 18:22:49 +0100

    Longer slideshow delay

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function() {
-  jQuery("#featured_holder").carousel( { direction: "vertical", autoSlide: true, pagination: true, autoSlideInterval: 3000 }  );
+  jQuery("#featured_holder").carousel( { direction: "vertical", autoSlide: true, pagination: true, autoSlideInterval: 10000 }  );
 })
