de0ea5f17d454b20d4878d69146b450029c53ae2
Amund <amunds@gmail.com>
2010-11-11 18:13:57 +0100

    Updated rooms template

--- room.php
+++ room.php
@@ -19,20 +19,18 @@ function custom_excerpt_length($length) {
 add_filter('excerpt_length', 'custom_excerpt_length');
 ?>
 
-  <div id="content" role="main">
-    <div id="left_content">
+  <div id="content" role="main" class="room_content">
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      
       <!-- This loops through all the children of the Rom page. -->
       <?php query_posts(array('post_parent' => 11, 'post_type' => 'page', 'post_status' => 'published', 'posts_per_page' => -1)); while (have_posts()) { the_post(); ?>
+        <div class="room_container">
+          <h2><?php the_title(); ?></h2>
+          <?php the_post_thumbnail('room-thumbnail'); ?>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_post_thumbnail('room-thumbnail'); ?>
-        <p><?php echo get_post_meta(get_the_ID(), 'details', true); ?></p>
-        <?php the_content(); ?>
-      <?php } ?>
-    </div>
-      <?php get_sidebar(); ?>
+          <?php the_content(); ?>
+          .:--:.
+        </div>
+        <?php } ?>
   </div><!-- #content -->
 
 <?php get_footer(); ?>
