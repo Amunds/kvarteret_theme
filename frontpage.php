24a133a0a4d0706166d33b0cf0550deb08584402
Amund <amunds@gmail.com>
2010-11-25 17:03:17 +0100

    get_pots by category_name

--- frontpage.php
+++ frontpage.php
@@ -23,8 +23,8 @@ add_filter('excerpt_length', 'custom_excerpt_length');
     <div id="left_content">
       <?php
         global $post;
-        $featured = get_posts('numberposts=5&category=5');
-        $current_news = get_posts('numberposts=10&category=4');
+        $featured = get_posts('numberposts=5&category_name="Featured"');
+        $current_news = get_posts('numberposts=10&category_name="Aktuelt"');
         ?>
         <div id="featured_holder">
           <ul>
