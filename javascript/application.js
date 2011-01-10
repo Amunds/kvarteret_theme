10a1eef9aba58d61cdf9d9f3eae935fb7e6fbee6
Amund <amunds@gmail.com>
2011-01-10 12:29:50 +0100

    longer time between slide changes

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function() {
-  jQuery("#featured_holder").carousel( { direction: "vertical", autoSlide: true, pagination: true, autoSlideInterval: 5000 }  );
+  jQuery("#featured_holder").carousel( { direction: "vertical", autoSlide: true, pagination: true, autoSlideInterval: 10000 }  );
 })
