1d958f301473648a970e3ed4ae39cf92f7a03558
Amund <amunds@gmail.com>
2011-01-10 17:22:56 +0100

    nothing found on search results page

--- search.php
+++ search.php
@@ -25,7 +25,7 @@ get_header(); ?>
 				?>
 <?php else : ?>
 				<div id="post-0" class="post no-results not-found">
-					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
+					<h2 class="entry-title"><?php _e( 'Ingenting funnet', 'twentyten' ); ?></h2>
 					<div class="entry-content">
 						<p><?php _e( 'Beklager, vi fant ingenting på søket ditt. Prøv noe annet?', 'twentyten' ); ?></p>
 						<?php get_search_form(); ?>
