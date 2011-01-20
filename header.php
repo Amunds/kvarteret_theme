5cced7af1043ed5810e6429d802d6e291deccda8
Amund <amunds@gmail.com>
2011-01-20 18:12:59 +0100

    google analytics

--- header.php
+++ header.php
@@ -50,6 +50,19 @@
     <script src="<?php bloginfo('template_directory'); ?>/javascript/application.js" 
       type="text/javascript" charset="utf-8">
     </script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-18345888-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
   </head>
 
   <body <?php body_class(); ?>>
