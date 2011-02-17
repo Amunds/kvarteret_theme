90e69a6082e69107c1b9391a3eb6bd8af7f760a5
Amund <amunds@gmail.com>
2011-02-17 16:07:09 +0100

    somewhat smaller font-size on container

--- samarbeidspartnere.php
+++ samarbeidspartnere.php
@@ -14,9 +14,9 @@ get_header(); ?>
 	<?php get_sidebar(); ?>
   <div id="content" role="main">
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      [<?php query_posts(array('post_parent' => 15, 'post_type' => 'page', 'post_status' => 'published', 'posts_per_page' => -1)); while (have_posts()) { the_post(); ?>
+      <?php query_posts(array('post_parent' => 15, 'post_type' => 'page', 'post_status' => 'published', 'posts_per_page' => -1)); while (have_posts()) { the_post(); ?>
         <a class="kultur_link" href="#<?php the_ID()?>"><?php the_title(); ?></a>
-        <?php } ?>]
+        <?php } ?>
       <!-- This loops through all the children of the samarbeidspartnere page. -->
       <?php query_posts(array('post_parent' => 15, 'post_type' => 'page', 'post_status' => 'published', 'posts_per_page' => -1)); while (have_posts()) { the_post(); ?>
         <div class="room_container">
