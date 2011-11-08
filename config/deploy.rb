ca514184de414b14a609cd591a37f5179eaeee03
Torstein Thune <torstein@gmail.com>
2011-11-08 23:30:37 +0100

    removed files added by mistake

--- config/deploy.rb
+++
@@ -1,4 +0,0 @@
-set :application, "kvarteret_theme"
-set :domain, "login.kvarteret.no"
-set :deploy_to, "/data/www/intern.kvarteret.no/infodev/git/#{application}"
-set :repository, 'git@github.com:Amunds/kvarteret_theme.git'
--- config/rake.rb
+++
@@ -1,6 +0,0 @@
-begin
-  require 'vlad'
-  Vlad.load
-rescue LoadError
-  # do nothing
-end
