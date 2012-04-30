d62f1f01346a906e2d8c75dfd202faa80e5d9b01
Endre Oma <eoma@kvarteret.no>
2012-04-30 20:10:21 +0200

    Add page sommerkvarteret

---
+++ sommerkvarteret.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template Name: Custom page for Sommerkvarteret
+ *
+ * A custom page template for the frontpage.
+ *
+ *
+ * @package WordPress
+ * @subpackage kvarteret
+ * @since Kvarteret 1.0
+ */
+
+get_header(); ?>
+<?php
+// Add custom excerpt length
+function custom_excerpt_length($length) {
+	return 20;
+}
+add_filter('excerpt_length', 'custom_excerpt_length');
+?>
+
+  <div id="content" role="main">
+    <div id="left_content">
+      <?php
+        global $post;
+        $current_news = get_posts('numberposts=10&category_name=sommerkvarteret');
+        ?>
+        <?php
+        foreach($current_news as $post) :
+          setup_postdata($post);
+        ?>
+      <div class="current_news">
+        <div class="current_news_image">
+          <?php
+          if ( has_post_thumbnail() ) {
+            ?>
+            <a href="<?php the_permalink(); ?>">
+            	<?php the_post_thumbnail( 'current-news-thumbnail' ); ?>
+            </a>
+            <?php
+          } else {
+          ?>
+          <a href="<?php the_permalink(); ?>">
+            <img src="<?php bloginfo('template_directory'); ?>/images/current_news_image_missing_<?php echo rand(1,2) ?>.png">
+          </a>
+          <?php
+          }
+          ?>
+        </div>
+        
+        <a href="<?php the_permalink(); ?>" class="current_news_title"><?php the_title(); ?></a>
+        <span class="excerpt">
+          <?php the_excerpt(); ?>
+        </span>
+      </div>
+        <?php endforeach; ?>
+    </div>
+     
+
+    <div id="standard_right_menu" class="widget-area" role="complementary">
+      <?php dynamic_sidebar( 'sommerkvarteret-widget-area' );  ?>
+    </div><!-- #sommerkvarteret .widget-area -->
+
+  </div><!-- #content -->
+
+<?php get_footer(); ?>
